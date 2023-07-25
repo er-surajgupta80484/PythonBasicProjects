9bb9ec79933480d40b322a40d72e708427c814ff
er-surajgupta80484 <125231532+er-surajgupta80484@users.noreply.github.com>
2023-07-25 18:33:55 +0530

    Add files via upload

---
+++ Tambola Number Counter.py
@@ -0,0 +1,35 @@
+#program to get a number to play tambola game between 0-90 and called number cant be repeated again
+
+import random
+numlist=[]
+print("Welcome to Tambola! Press enter to start the game and type quit & hit enter to quit\n\n")
+choice=input("Press enter to start the game")
+loop_input=''
+loop_counter=0
+if choice=='':
+    while loop_input!='quit':
+        num=random.randint(1,90)
+        if num in numlist:
+            continue
+        else:
+            loop_input = input("Press enter to generate new number")
+            if loop_input == '':
+                print(num)
+                numlist.append(num)
+                loop_counter+=1
+
+            if loop_input=='quit' or loop_counter==90:
+                break
+print(f"Game Over! All 90 numbers have been called out\n\nnumbers called are {numlist}")
+#below is the sample code to test if numbers are all unique by converting list to set and match the count of elements in set and list. because set contains unique values only.
+#count=0
+#myset=set()
+#for element in numlist:
+#    count+=1
+ #   myset.add(element)
+#print(count)
+#count=0
+#for element in myset:
+ #   count+=1
+#print(count)
+
